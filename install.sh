1f74e30160a7843b8660f226fe39fe5647fa8a47
Alisson Moretto <4w4k3@users.noreply.github.com>
2017-05-15 09:45:19 -0300

    Updated Install Method

--- install.sh
+++ install.sh
@@ -1,8 +1,4 @@
-sudo dpkg --add-architecture i386
-sudo apt-get update --fix-missing
-sudo apt-get dist-upgrade -yy && sudo apt-get upgrade -yy
-sudo apt-get -f install
-sudo apt-get install wine -y && sudo apt-get install wine32 -y && sudo apt-get install winbind -y
+sudo dpkg --add-architecture i386 && sudo apt-get update && apt-get install wine32
 sudo winecfg
 sudo wine msiexec /i python-2.7.12.msi /L*v log.txt
 sudo wine pywin32-220.win32-py2.7.exe
