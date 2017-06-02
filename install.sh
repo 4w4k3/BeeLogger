2c8a1da7936ce0815a0df85d9e83ad68d1b7fa76
Alisson Moretto <4w4k3@users.noreply.github.com>
2017-06-02 13:38:21 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
-sudo dpkg --add-architecture i386 && sudo apt-get update && apt-get install wine32
-sudo winecfg
-sudo wine msiexec /i python-2.7.12.msi /L*v log.txt
-sudo wine pywin32-220.win32-py2.7.exe
-sudo wine pyHook-1.5.1.win32-py2.7.exe
-sudo wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller
+dpkg --add-architecture i386 && apt-get update && apt-get install  -y
+winecfg
+wine msiexec /i python-2.7.12.msi /L*v log.txt
+wine pywin32-220.win32-py2.7.exe
+wine pyHook-1.5.1.win32-py2.7.exe
+wine /root/.wine/drive_c/Python27/python.exe -m pip install pyinstaller
